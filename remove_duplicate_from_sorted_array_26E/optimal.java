2d9f559cfca621f0fd86215ee95446737cf18ae4
Mukesh <mukesh.sah@msg-global.com>
2025-07-17 07:46:25 +0530

    added remove_duplicates

--- remove_duplicate_from_sorted_array_26E/optimal.java
+++ remove_duplicate_from_sorted_array_26E/optimal.java
@@ -1,21 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int[] temp=new int[nums.length];
-        temp[0]=nums[0];
         int rd=0;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]==temp[rd]){
+            if(nums[i]==nums[rd]){
                 continue;
             }
             else{
                 rd++;
-                temp[rd]=nums[i];
+                nums[rd]=nums[i];
 
             }
-        }
-
-        for(int i=0;i<=rd;i++){
-            nums[i]=temp[i];
         }
         return rd+1;
     }
